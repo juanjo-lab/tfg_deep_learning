519545bfca7cc475422c06f28fe800822c687442
juanjo-lab <61283185+juanjo-lab@users.noreply.github.com>
2022-06-19 17:56:58 +0200

    Delete Crear_red directory

--- Crear_red/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-
-
-class Document(models.Model):
-    docfile = models.FileField(upload_to='Crear_Red/juanjofb/')
-
--- Crear_red/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import include, url
-from Crear_red.views import *
-import Crear_red.views as views
-
-urlpatterns = [
-    url(r'^Crear_red/$', views.list, name='list_cre')
-]
