0dab1088a61ac4d57aaa1f1b889ce479ff7aa57e
juanjo-lab <61283185+juanjo-lab@users.noreply.github.com>
2022-06-24 00:12:14 +0200

    Add files via upload

---
+++ Predecir/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, url
+from Predecir.views import *
+import Predecir.views as views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    url(r'^Predecir/$', views.list, name='list_pre')
+]
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ Re_entrenar/apps.py
@@ -0,0 +1,13 @@
+'''
+Librerias emplementadas para el modulo de ususario, en esta sección no se requiere ninguna modificacion para el usuario
+que desee implementar la aplicación.
+'''
+########################################################################################################################
+from __future__ import unicode_literals
+from django.apps import AppConfig
+########################################################################################################################
+
+########################################################################################################################
+class UploadConfig(AppConfig):
+    name = 'Re_entrenar'
+########################################################################################################################
