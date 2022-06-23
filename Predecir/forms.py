32c6a5db3a4f9526935c34ff5485fa75f3b59ac8
juanjo-lab <61283185+juanjo-lab@users.noreply.github.com>
2022-06-24 00:09:36 +0200

    Delete Predecir directory

--- Predecir/forms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django import forms
-
-class DocumentForm(forms.Form):
-    docfile = forms.FileField(
-        label='Seleccionar el archivo: ', help_text = 'username-test.csv'
-    )
